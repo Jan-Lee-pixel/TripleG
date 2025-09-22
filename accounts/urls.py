633f27744c2014e845930c4c140555ef7f09d54d
demesis221 <rideouts199@gmail.com>
2025-09-22 10:56:13 +0800

    finalize all adminside and template moves

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
- ]
+    # Admin
+    path('admin/login/', views.admin_login_view, name='admin_login'),
+    path('admin/register/', views.admin_register_view, name='admin_register'),
+
+    # Client
+    path('client/login/', views.client_login_view, name='client_login'),
+    path('client/register/', views.client_register_view, name='client_register'),
+
+    # Sitemanger
+    path('sitemanger/login/', views.sitemanger_login_view, name='sitemanger_login'),
+    path('sitemanger/register/', views.sitemanger_register_view, name='sitemanger_register'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
+
+
+# User Login View
 from django.shortcuts import render
 
-# Create your views here.
+# Admin
+
+def admin_login_view(request):
+    return render(request, 'admin/login.html')
+
+def admin_register_view(request):
+    return render(request, 'admin/register.html')
+
+# Client
+
+def client_login_view(request):
+    return render(request, 'client/login.html')
+
+def client_register_view(request):
+    return render(request, 'client/register.html')
+
+# Sitemanger
+
+def sitemanger_login_view(request):
+    return render(request, 'sitemanger/login.html')
+
+def sitemanger_register_view(request):
+    return render(request, 'sitemanger/register.html')
